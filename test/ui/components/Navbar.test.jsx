427588a90ffbc458f9e2a6d37cc69926ae545799
Gastón Pappaterra <gpappaterra@hotmail.com>
2022-10-09 15:51:27 +0000

    Configure test code sandbox

---
+++ test/ui/components/Navbar.test.jsx
@@ -0,0 +1,4 @@
+describe("Pruebas en Navbar", () => {
+  test("debe mostrar el nombre del usuario", () => {});
+  test("debe de llamar al logout y el navigate cuando se hace click en el boton", () => {});
+});
